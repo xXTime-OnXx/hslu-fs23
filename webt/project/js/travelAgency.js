923339c5f235f9a9b5ab3832024d82edb00a4e50
Timon Schmid <timon.schmid@bluewin.ch>
2023-06-05 11:13:54 +0200

    add clientside validation & persistence

--- webt/project/js/travelAgency.js
+++ webt/project/js/travelAgency.js
@@ -1,14 +1,56 @@
+function formatDate(date) {
+    return date.toISOString().split('T')[0];
+}
+
 Vue.createApp({
     data() {
         return {
              travelplan: {
-                firstname: '',
-                lastname: '',
-                email: '',
-                destination: '',
-                startDate: Date.now(),
-                endDate: Date.now()
+                firstname: undefined,
+                lastname: undefined,
+                email: undefined,
+                destination: undefined,
+                startDate: undefined,
+                endDate: undefined
              }
         }
+    },
+    methods: {
+        validEmail() {
+            if (!this.travelplan.email) {
+                return false;
+            }
+            return this.travelplan.email.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/);
+        },
+        validStartDate() {
+            if (!this.travelplan.startDate) {
+                return false;
+            }
+            const validDate = new Date();
+            validDate.setHours(0, 0, 0, 0);
+            validDate.setDate(validDate.getDate() + 7);
+            const startDate = new Date(this.travelplan.startDate);
+            return startDate >= validDate;
+        },
+        validEndDate() {
+            if (!this.travelplan.endDate) {
+                return false;
+            }
+            const startDate = new Date(this.travelplan.startDate);
+            const endDate = new Date(this.travelplan.endDate);
+            return endDate > startDate;
+        },
+        validForm() {
+            console.log('form validator');
+            return false;
+        }
+    },
+    mounted() {
+        const defaultDate = new Date();
+        defaultDate.setDate(defaultDate.getDate() + 7);
+        this.travelplan.startDate = formatDate(defaultDate);
+
+        defaultDate.setDate(defaultDate.getDate() + 1);
+        this.travelplan.endDate = formatDate(defaultDate);
     }
 }).mount('#app');
--- webt/project/travel_plan.php
+++ webt/project/travel_plan.php
@@ -5,49 +5,57 @@
     <title>Berechneter Gesamtwiderstand</title>
     <meta charset="utf8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 </head>
 
 <body>
-    <?php
-    $hostname = 'localhost';
-    $username = 'root';
-    $password = '';
-    $database = 'travel_agency';
+    <main class="w3-container">
+        <?php
+        $hostname = 'localhost';
+        $username = 'root';
+        $password = '';
+        $database = 'travel_agency';
 
-    $conn = mysqli_connect($hostname, $username, $password, $database);
-    if (!$conn) {
-        die('Database connection failed');
-    }
+        $conn = mysqli_connect($hostname, $username, $password, $database);
+        if (!$conn) {
+            die('Database connection failed');
+        }
 
-    $firstname = $_POST['firstname'];
-    $lastname = $_POST['lastname'];
-    $email = $_POST['email'];
-    $destination = $_POST['destination'];
-    $startDate = $_POST['startDate'];
-    $endDate = $_POST['endDate'];
+        $firstname = $_POST['firstname'];
+        $lastname = $_POST['lastname'];
+        $email = $_POST['email'];
+        $destination = $_POST['destination'];
+        $startDate = $_POST['startDate'];
+        $endDate = $_POST['endDate'];
 
-    // TODO: validation
+        // TODO: validation
+        
+        $firstname = mysqli_real_escape_string($conn, $firstname);
+        $lastname = mysqli_real_escape_string($conn, $lastname);
+        $email = mysqli_real_escape_string($conn, $email);
+        $destination = mysqli_real_escape_string($conn, $destination);
+        $startDate = mysqli_real_escape_string($conn, $startDate);
+        $endDate = mysqli_real_escape_string($conn, $endDate);
 
-    $firstname = mysqli_real_escape_string($conn, $firstname);
-    $lastname = mysqli_real_escape_string($conn, $lastname);
-    $email = mysqli_real_escape_string($conn, $email);
-    $destination = mysqli_real_escape_string($conn, $destination);
-    $startDate = mysqli_real_escape_string($conn, $startDate);
-    $endDate = mysqli_real_escape_string($conn, $endDate);
+        $query = "INSERT INTO travel_plan (firstname, lastname, email, destination, start_date, end_date) VALUES (?, ?, ?, ?, ?, ?)";
+        $stmt = mysqli_prepare($conn, $query);
+        mysqli_stmt_bind_param($stmt, 'ssssss', $firstname, $lastname, $email, $destination, $startDate, $endDate);
 
-    $query = "INSERT INTO travel_plan (firstname, lastname, email, destination, start_date, end_date) VALUES (?, ?, ?, ?, ?, ?)";
-    $stmt = mysqli_prepare($conn, $query);
-    mysqli_stmt_bind_param($stmt, 'ssssss', $firstname, $lastname, $email, $destination, $startDate, $endDate);
+        $statementExecuted = mysqli_stmt_execute($stmt);
+        if (!$statementExecuted) {
+            die("Statement execution failed");
+        }
 
-    $statementExecuted = mysqli_stmt_execute($stmt);
-    if (!$statementExecuted) {
-        die ("Statement execution failed");
-    }
+        echo "<h2>Ihr Reiseplanungs Antrag wurde erfolgreich erstellt</h2>";
+        echo "<h3>Reiseplanung:</h3>";
+        echo "<p>Name: " . $firstname . " " . $lastname . "</p>";
+        echo "<p>E-Mail: " . $email . "</p>";
+        echo "<p>Destination: " . $destination . "</p>";
+        echo "<p>Dauer: " . $startDate . " - " . $endDate . "</p>";
 
-    // TODO: create response with form data
-
-    ?>
-    <a href="travelAgency.html">Zurück zur Website</a>
+        ?>
+        <a href="travelAgency.html">Zurück zur Website</a>
+    </main>
 </body>
 
 </html>

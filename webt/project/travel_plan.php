f2e52198ac11648f865e1414cf2b99acc1d78dbb
Timon Schmid <timon.schmid@bluewin.ch>
2023-06-16 15:54:22 +0200

    renaming

--- webt/project/travel_plan.php
+++ webt/project/travel_plan.php
@@ -26,6 +26,9 @@
         function isValidEndDate($startDate, $endDate) {
             return $endDate > $startDate;
         }
+        function returnToWebsiteTag() {
+            return '<a href="index.html">Zurück zur Website</a>';
+        }
 
         $hostname = 'localhost';
         $username = 'root';
@@ -45,22 +48,22 @@
         $endDate = $_POST['endDate'];
 
         if (isNullOrEmptyString($firstname)) {
-            die('Der Vorname darf nicht leer sein');
+            die('Der Vorname darf nicht leer sein' + returnToWebsiteTag());
         }
         if (isNullOrEmptyString($lastname)) {
-            die('Der Nachname darf nicht leer sein');
+            die('Der Nachname darf nicht leer sein' + returnToWebsiteTag());
         }
         if (!isValidEmail($email)) {
-            die('Die Email ist ungültig');
+            die('Die Email ist ungültig' + returnToWebsiteTag());
         }
         if (isNullOrEmptyString($destination) || strlen($destination) < 5) {
-            die('Die Beschreibung der Destination ist zu kurz');
+            die('Die Beschreibung der Destination ist zu kurz' + returnToWebsiteTag());
         }
         if (!isValidStartDate($startDate)) {
-            die('Das Start Datum muss mindestens 7 Tage in der Zukunft liegen');
+            die('Das Start Datum muss mindestens 7 Tage in der Zukunft liegen' + returnToWebsiteTag());
         }
         if (!isValidEndDate($startDate, $endDate)) {
-            die('Das ende Datum muss nach dem start Datum sein');
+            die('Das ende Datum muss nach dem start Datum sein' + returnToWebsiteTag());
         }
         
         $firstname = mysqli_real_escape_string($conn, $firstname);
@@ -80,7 +83,14 @@
         }
         mysqli_close($conn);
 
-        echo '<h2>Ihr Reiseplanungs Antrag wurde erfolgreich erstellt</h2>';
+        if(isset($_COOKIE["travel_plan_count"])){
+            $count = $_COOKIE["travel_plan_count"] + 1;
+        } else {
+            $count = 1;
+        }
+        setcookie("travel_plan_count", $count, time() + (10 * 365 * 24 * 60 * 60), "/");
+
+        echo '<h2>Ihr' . $count . '. Reiseplanungs Antrag wurde erfolgreich erstellt</h2>';
         echo '<h3>Reiseplanung:</h3>';
         echo '<p>Name: ' . $firstname . ' ' . $lastname . '</p>';
         echo '<p>E-Mail: ' . $email . '</p>';
@@ -88,7 +98,7 @@
         echo '<p>Dauer: ' . $startDate . ' - ' . $endDate . '</p>';
 
         ?>
-        <a href="travelAgency.html">Zurück zur Website</a>
+        <a href="index.html">Zurück zur Website</a>
     </main>
 </body>
 

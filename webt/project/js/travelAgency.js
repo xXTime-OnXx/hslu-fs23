5a8c9a353ab083525217f51c614b532aab0233c3
Timon Schmid <timon.schmid@bluewin.ch>
2023-06-16 08:45:32 +0200

    add validation and canvas animation

--- webt/project/js/travelAgency.js
+++ webt/project/js/travelAgency.js
@@ -5,14 +5,19 @@ function formatDate(date) {
 Vue.createApp({
     data() {
         return {
-             travelplan: {
+            travelplan: {
                 firstname: undefined,
                 lastname: undefined,
                 email: undefined,
                 destination: undefined,
                 startDate: undefined,
                 endDate: undefined
-             }
+            },
+            plane: {
+                image: new Image(),
+                x: 5,
+                y: 20
+            }
         }
     },
     methods: {
@@ -40,9 +45,52 @@ Vue.createApp({
             const endDate = new Date(this.travelplan.endDate);
             return endDate > startDate;
         },
-        validForm() {
-            console.log('form validator');
-            return false;
+        animatePlane() {
+            const ctx = document.getElementById("canvas").getContext("2d");
+
+            // Start point
+            ctx.beginPath()
+            ctx.arc(6, 20 + 32, 5, 0, 2 * Math.PI);
+            ctx.fill();
+
+            // Destination point
+            ctx.beginPath()
+            ctx.arc(225, 20 + 32, 5, 0, 2 * Math.PI);
+            ctx.fill();
+
+            // Move plane
+            prevX = this.plane.x;
+            prevY = this.plane.y;
+            ctx.clearRect(this.plane.x, this.plane.y, 64, 64);
+            if (this.plane.x < 225) {
+                this.plane.x += 1;
+            }
+            if (this.plane.x < 60) {
+                this.plane.y -= 0.5;
+            }
+            if (this.plane.x > 170 && this.plane.x < 225) {
+                this.plane.y += 0.5;
+            }
+
+            // Add route after plane
+            ctx.strokeStyle = "grey";
+            ctx.drawImage(this.plane.image, this.plane.x, this.plane.y, 64, 64);
+            ctx.beginPath()
+            ctx.moveTo(prevX, prevY + 32);
+            ctx.lineTo(this.plane.x, this.plane.y + 32);
+            ctx.stroke();
+
+            // restart animation
+            if (this.plane.x > 224) {
+                setTimeout(() => {
+                    ctx.clearRect(0, 0, 300, 74);
+                    this.plane.x = 5;
+                    this.plane.y = 20;
+                }, 1000);
+            }
+            setTimeout(() => {
+                this.animatePlane();
+            }, 50);
         }
     },
     mounted() {
@@ -52,5 +100,8 @@ Vue.createApp({
 
         defaultDate.setDate(defaultDate.getDate() + 1);
         this.travelplan.endDate = formatDate(defaultDate);
+
+        this.plane.image.src = "./img/plane.png";
+        this.animatePlane();
     }
 }).mount('#app');
--- webt/project/travel_plan.php
+++ webt/project/travel_plan.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <title>Berechneter Gesamtwiderstand</title>
+    <title>Reiseplanungs Antrag</title>
     <meta charset="utf8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
@@ -11,6 +11,22 @@
 <body>
     <main class="w3-container">
         <?php
+        function IsNullOrEmptyString($str) {
+            return ($str === null || trim($str) === '');
+        }
+        function isValidEmail($email) {
+            $emailPattern = '/^[\w\-\.]+@([\w\-]+\.)+[\w\-]{2,4}$/i';
+            return preg_match($emailPattern, $email);
+        }
+        function isValidStartDate($date) {
+            $validDate = date_create();
+            date_add($validDate, date_interval_create_from_date_string("7 days"));
+            return $date >= date_format($validDate, 'Y-m-d');
+        }
+        function isValidEndDate($startDate, $endDate) {
+            return $endDate > $startDate;
+        }
+
         $hostname = 'localhost';
         $username = 'root';
         $password = '';
@@ -28,7 +44,24 @@
         $startDate = $_POST['startDate'];
         $endDate = $_POST['endDate'];
 
-        // TODO: validation
+        if (isNullOrEmptyString($firstname)) {
+            die('Der Vorname darf nicht leer sein');
+        }
+        if (isNullOrEmptyString($lastname)) {
+            die('Der Nachname darf nicht leer sein');
+        }
+        if (!isValidEmail($email)) {
+            die('Die Email ist ungültig');
+        }
+        if (isNullOrEmptyString($destination) || strlen($destination) < 5) {
+            die('Die Beschreibung der Destination ist zu kurz');
+        }
+        if (!isValidStartDate($startDate)) {
+            die('Das Start Datum muss mindestens 7 Tage in der Zukunft liegen');
+        }
+        if (!isValidEndDate($startDate, $endDate)) {
+            die('Das ende Datum muss nach dem start Datum sein');
+        }
         
         $firstname = mysqli_real_escape_string($conn, $firstname);
         $lastname = mysqli_real_escape_string($conn, $lastname);
@@ -37,21 +70,22 @@
         $startDate = mysqli_real_escape_string($conn, $startDate);
         $endDate = mysqli_real_escape_string($conn, $endDate);
 
-        $query = "INSERT INTO travel_plan (firstname, lastname, email, destination, start_date, end_date) VALUES (?, ?, ?, ?, ?, ?)";
+        $query = 'INSERT INTO travel_plan (firstname, lastname, email, destination, start_date, end_date) VALUES (?, ?, ?, ?, ?, ?)';
         $stmt = mysqli_prepare($conn, $query);
         mysqli_stmt_bind_param($stmt, 'ssssss', $firstname, $lastname, $email, $destination, $startDate, $endDate);
 
         $statementExecuted = mysqli_stmt_execute($stmt);
         if (!$statementExecuted) {
-            die("Statement execution failed");
+            die('Statement execution failed');
         }
+        mysqli_close($conn);
 
-        echo "<h2>Ihr Reiseplanungs Antrag wurde erfolgreich erstellt</h2>";
-        echo "<h3>Reiseplanung:</h3>";
-        echo "<p>Name: " . $firstname . " " . $lastname . "</p>";
-        echo "<p>E-Mail: " . $email . "</p>";
-        echo "<p>Destination: " . $destination . "</p>";
-        echo "<p>Dauer: " . $startDate . " - " . $endDate . "</p>";
+        echo '<h2>Ihr Reiseplanungs Antrag wurde erfolgreich erstellt</h2>';
+        echo '<h3>Reiseplanung:</h3>';
+        echo '<p>Name: ' . $firstname . ' ' . $lastname . '</p>';
+        echo '<p>E-Mail: ' . $email . '</p>';
+        echo '<p>Destination: ' . $destination . '</p>';
+        echo '<p>Dauer: ' . $startDate . ' - ' . $endDate . '</p>';
 
         ?>
         <a href="travelAgency.html">Zurück zur Website</a>
